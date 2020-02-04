eacf10cf5b7205a902abaf99570882e297c942f3
danishsshaikh <danishsshaikh@gmail.com>
2020-02-05 01:01:54 +0530

    Create iris.py

---
+++ iris.py
@@ -0,0 +1,61 @@
+# Load libraries
+from pandas import read_csv 
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+# Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = read_csv(url, names=names)
+
+#<----------------- Summarizing the Data Set-------------->
+# shape
+# .shape is used to get the idea of rows and columns from the data set
+# we use .shape
+# print(dataset.shape)
+
+# head
+# .head() is ued to peek at the actual data with names after 'read_csv'
+# print(dataset.head(20))
+
+# descriptions
+# .describe is used to peek at count,mean,min,max value and %
+# print(dataset.describe())
+
+# class distribution
+# print(dataset.groupby('class').size())
+
+#<----------------- Data Visualization-------------->
+
+#Plots:
+#   1. Univariate Plots     #
+
+# Box and Whisker plots
+# dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+# pyplot.show()
+
+# Histograms
+# dataset.hist()
+# pyplot.show()
+
+#   2. Mulrivariate Plots   #
+
+# Scatter plot matrix
+# scatter_matrix(dataset)
+# pyplot.show()
+
+#<----------------- Evaluating Algo's-------------->
+
+
